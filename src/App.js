fc70c05d2029bcb222b0887281cd4ba8977ccb16
Pratik Awate <awatepratik27@gmail.com>
2020-09-07 15:59:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit12 <code>src/App.js</code> and should pratik not save to reload.
         </p>
         <a
           className="App-link"
